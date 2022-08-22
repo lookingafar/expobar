03890739b6e3e55ea6e4ec0ff977c86bda419b49
looking_afar <m.e.karimov@gmail.com>
2022-08-22 15:50:24 +0800

    Add GPIO cleanup

--- group_temperature.py
+++ group_temperature.py
@@ -26,12 +26,10 @@ else:
     from emulated_hx711 import HX711
 
 def cleanAndExit():
-    print("Cleaning...")
 
     if not EMULATE_HX711:
         GPIO.cleanup()
         
-    print("Bye!")
     sys.exit()
 
 hx = HX711(5, 6)
@@ -131,6 +129,7 @@ async def main():
 
  except KeyboardInterrupt:
     disp.module_exit()
+    cleanAndExit()
     logging.info("quit:")
     exit()
 asyncio.run(main())
